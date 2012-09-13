107999c6671680e2484188dd2ddd3754c09f18dc
Mikkel Krautz <mikkel@krautz.dk>
2012-09-13 20:22:29 +0200

    objc: add LICENSE.

--- demo/demo.m
+++ demo/demo.m
@@ -1,3 +1,7 @@
+// Copyright (c) 2012 The 'objc' Package Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // clang demo.m -framework Foundation -framework AppKit -o demo-clang
 
 #import <Foundation/Foundation.h>
--- object.go
+++ object.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2012 The 'objc' Package Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Package objc implements access to the Objective-C runtime from Go
 package objc
 
